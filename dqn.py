77a9395c8c1b250857d100fc612b0e8868d74857
ul1234 <unclelim@sina.com>
2019-03-24 23:28:31 +0800

    1

--- dqn.py
+++ dqn.py
@@ -7,6 +7,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
+from collections import namedtuple
+import random
 
 
 class QNet(nn.Module):
@@ -17,13 +19,36 @@ class QNet(nn.Module):
         self.fc1 = nn.Linear(in_neurons, 30)
         self.fc2 = nn.Linear(30, 1)   # output Q value
 
-    def forward(self, observation, actions):
-        num_actions = len(actions) if isinstance(actions, list) else 1
-        x = np.concatenate((np.tile(np.array(observation), (num_actions, 1)), np.array(actions).reshape(-1,1)), axis = 1)
+    def forward(self, x):
         x = torch.from_numpy(x.astype(np.float32))
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
+class RecordBuffer(object):
+    def __init__(self, buffer_size = 1000):
+        self.record = namedtuple('record', 'observation action next_observation reward')
+        self.buffer_size = buffer_size
+        self.buffer_write_index = 0
+        self.buffer = []
+
+    def save(self, *record_elements):
+        # transition: observation, action -> observation(t+1), reward
+        record = self.record(*record_elements)
+        if not self.is_full():
+            self.buffer.append(record)
+        else:
+            self.buffer[self.buffer_write_index] = record
+            self.buffer_write_index = (self.buffer_write_index + 1) % self.buffer_size
+
+    def sample(self, batch_size):
+        assert batch_size < len(self.buffer), 'invalid batch_size'
+        records = random.sample(self.buffer, batch_size)
+        return records
+
+    def is_full(self):
+        return len(self.buffer) >= self.buffer_size
+
 
 class DQN(object):
     def __init__(self, game, gamma = 0.9, greedy = 0.1):
@@ -37,6 +62,7 @@ class DQN(object):
         self.greedy = greedy
         self.criterion = nn.MSELoss()
         self.optimizer = optim.SGD(self.qnet_train.parameters(), lr = 0.01)
+        self.record_buffer = RecordBuffer(buffer_size = 1000)
 
     def train(self, epoches):
         print('start to train...')
@@ -48,17 +74,29 @@ class DQN(object):
             rewards = 0
             while not done:
                 action = self.choose_action(observation)
-                q_eval = self.qnet_train(observation, action)
-                observation, reward, done, info = self.env.step(action)
-                with torch.no_grad():
-                    q_target = torch.max(self.qnet_target(observation, self.actions))
-                # Q learning
-                # Q(state, action) = reward + gamma * max(Q(state+1, action_space))
-                loss = self.criterion(q_eval, q_target * self.gamma + reward)
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-                num_updates += 1
+                next_observation, reward, done, info = self.env.step(action)
+
+                self.record_buffer.save(observation, action, next_observation, reward)
+
+                if self.record_buffer.is_full():
+                    records = self.record_buffer.sample(batch_size = 20)
+                    q_eval_x = np.array([np.concatenate((ob, np.array([act]))) for ob, act, next, reward in records])
+                    num_actions = len(self.actions)
+                    q_target_x = np.array([np.concatenate((np.tile(next, (num_actions, 1)), np.array(actions).reshape(-1,1)), axis = 1)
+                        for ob, act, next, reward in records])
+                    rewards_target = np.array([reward for ob, act, next, reward in records])
+
+                    # Q learning
+                    # Q(state, action) = reward + gamma * max(Q(state+1, action_space))
+                    q_eval = self.qnet_train(q_eval_x)
+                    with torch.no_grad():
+                        q_target = torch.max(self.qnet_target(q_target_x).view(-1, 2)) * self.gamma + rewards_target
+
+                    loss = self.criterion(q_eval, q_target)
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+                    num_updates += 1
                 num_steps += 1
                 rewards += reward
             print('epoch {}: steps {}, rewards: {}'.format(epoch, num_steps, rewards))
@@ -72,7 +110,9 @@ class DQN(object):
         if explore:
             action = self.env.action_space.sample()
         else:
-            action = self.actions[torch.argmax(self.qnet_train(observation, self.actions)).item()]
+            num_actions = len(self.actions)
+            q_target_x = np.concatenate((np.tile(observation, (num_actions, 1)), np.array(self.actions).reshape(-1,1)), axis = 1)
+            action = self.actions[torch.argmax(self.qnet_train(q_target_x)).item()]
         return action
 
     def run(self, epoches):
