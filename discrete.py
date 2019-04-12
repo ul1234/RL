3272586c65816ea3c8ba38cb41b659b4318b7a27
unclelim <unclelim@sina.com>
2019-04-12 18:25:20 +0800

    '1'

--- discrete.py
+++ discrete.py
@@ -141,10 +141,10 @@ class Agent(object):
 class Game(object):
     def __init__(self, game_name):
         self.env = gym.make(game_name)
-        self.env.seed(1)
+        #self.env.seed(1)
         print('action space:', self.env.action_space)
         print('observation space:', self.env.observation_space)
-        self.agent = Agent(self.env.observation_space, self.env.action_space)
+        self.agent = Agent(self.env.observation_space.n, self.env.action_space.n)
         #self.reward_shaping = getattr(self, 'reward_shaping_{}'.format(game_name.split('-')[0]))
         self.resolved = getattr(self, 'resolved_{}'.format(game_name.split('-')[0]))
 
@@ -167,30 +167,38 @@ class Game(object):
             return True
         return False
 
-    def run(self, episodes, optimal = False, render = False):
+	def resolved_Taxi(self, scores):
+		return False
+
+	def run_one_episode(self, optimal = False, render = False):
+        observation = self.env.reset()
+        done = False
+        num_steps = 0
+        shaping_rewards = 0
+        rewards = 0
+        while not done:
+            if render: self.env.render()
+            action = self.agent.act(observation, optimal)
+            next_observation, reward, done, _ = self.env.step(action)
+            shaping_reward = reward if not hasattr(self, 'reward_shaping') else self.reward_shaping(observation, next_observation, reward)
+            if not optimal: self.agent.step(observation, action, next_observation, shaping_reward, done)
+            observation = next_observation
+            num_steps += 1
+            rewards += reward
+            shaping_rewards += shaping_reward
+        return rewards, shaping_rewards, num_steps
+
+	def run(self, episodes, optimal = False, render = False):
         print('start to run (optimal: {})...'.format(optimal))
+        shaping_scores = []
         scores = []
+        shaping_scores = []
         for episode in range(episodes):
-            actions_history = []
-            observation = self.env.reset()
             self.agent.new_episode()
-            done = False
-            num_steps = 0
-            rewards = 0
-            while not done:
-                if render: self.env.render()
-                action = self.agent.choose_action(observation, optimal = optimal)
-                actions_history.append(action)
-                next_observation, reward, done, _ = self.env.step(action)
-                shaping_reward = reward if not hasattr(self, 'reward_shaping') else self.reward_shaping(next_observation, reward)
-                if not optimal: self.agent.memorize(observation, action, next_observation, shaping_reward, done)
-                observation = next_observation
-                num_steps += 1
-                rewards += reward
-            #print('episode {}: steps {}, rewards: {}, loss: {}'.format(episode, num_steps, rewards, self.agent.loss_history))
-            #print('episode {}: steps {}, rewards: {}, actions: {}'.format(episode, num_steps, rewards, actions_history))
-            print('episode {}: steps {}, rewards: {}'.format(episode, num_steps, rewards))
+            rewards, shaping_rewards, num_steps = self.run_one_episode(optimal, render)
+            print('episode {}: steps {}, rewards: {}, shaping_rewards: {}, num_learns: {}'.format(episode, num_steps, rewards, shaping_rewards, self.agent.num_learns))
             scores.append(rewards)
+            shaping_scores.append(shaping_rewards)
             if self.resolved(scores): break
             #if episode % 10 == 0:
             #    plt.plot(self.agent.loss_history)
@@ -198,15 +206,19 @@ class Game(object):
             #if episode > 300: optimal = True
             #if rewards == 200: render = True
         plt.plot(scores)
+        plt.plot(shaping_scores, 'r--')
         plt.show()
+        pprint.pprint(self.agent.debug_rewards)
+
 
 
 if __name__ == '__main__':
-    game = Game('CartPole-v0')
+    #game = Game('CartPole-v0')
     #game = Game('MountainCar-v0')
+	game = Game('Taxi-v2')
 
     game.run(episodes = 1000)
-    game.agent.save()
+    #game.agent.save()
     #game.agent.load()
     #game.replay_buffer.show()
     #game.run(episodes = 1, optimal = True, render = True)
